f0bd92a2ed9699fd0930631154f10c50cfc033a9
Ayanava Karmakar <ayanava@rapidken.ai>
2023-07-22 17:20:15 +0530

    feat: add servlet for addition of two numbers

---
+++ src/main/java/software/ayanavakarmakar/servlejspref/AddServlet.java
@@ -0,0 +1,17 @@
+package software.ayanavakarmakar.servlejspref;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AddServlet extends HttpServlet {
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+        PrintWriter out = response.getWriter();
+        out.println("The result is " + (num1 + num2));
+    }
+}
